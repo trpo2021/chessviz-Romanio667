a144b88399223dad451a9cfa2dbda12900bed5ed
Мирошниченко Роман Олегович <Roman@localhost.localdomain>
2021-05-10 19:22:42 +0700

    change style

--- test/move-test.c
+++ test/move-test.c
@@ -4,8 +4,8 @@
 
 CTEST(test, move_test)
 {
-    int i,j,j1=2, j2=4;
-    char i1='e', i2='e';
+    int i, j, j1 = 2, j2 = 4;
+    char i1 = 'e', i2 = 'e';
     char realchess[10][10]
             = {"1rnbqkbnr",
                "2pppppppp",
@@ -18,7 +18,7 @@ CTEST(test, move_test)
                " ABCDEFGH"};
     char movement[5] = {i1, j1, i2, j2};
     move(realchess, 1, movement);
-    i = (int)(i2 - 'a')-1;
-    j = j2+1;
+    i = (int)(i2 - 'a') - 1;
+    j = j2 + 1;
     ASSERT_EQUAL((char)realchess[i][j], 'p');
 }
